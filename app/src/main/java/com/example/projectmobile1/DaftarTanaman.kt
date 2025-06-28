1951e45142676546ebd661f3c2c4b3f27d58ea46
Rasyd <rasyidridhotv@gmail.com>
2025-06-28 21:51:33 +0700

    update paling terbaru

---
+++ app/src/main/java/com/example/projectmobile1/DaftarTanaman.kt
@@ -0,0 +1,20 @@
+package com.example.projectmobile1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DaftarTanaman : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_daftar_tanaman)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
